0d6766b1db8059f137f34e6a224faf3476e5b334
Mark Challoner <mark.a.r.challoner@gmail.com>
2020-06-10 20:15:47 +0100

    Autocommit on mark-smarkets-pc-b

--- mark-smarkets-pc-b/bin/disable-usb-wakeup.sh
+++ mark-smarkets-pc-b/bin/disable-usb-wakeup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Get devices where wakeup enabled.
-device_range=$(grep -l enabled /sys/bus/usb/devices/*/power/wakeup | sed 's#/sys/bus/usb/devices/\([^/]\+\)/power/wakeup#\1#g' | tr '-' ' ')
+device_ranges=$({ grep -l enabled /sys/bus/usb/devices/*/power/wakeup || true; } | sed 's#/sys/bus/usb/devices/\([^/]\+\)/power/wakeup#\1#g')
+# Check if we need to continue.
+if [ -z "$device_ranges" ]; then
+  echo "No USB devices have wake enabled."
+  exit
+fi
 # Login.
 sudo -v
 # Disable device wakeup.
-while read device_id; do
+while read device_range; do
   # Set device wakeup to disabled.
-  echo -n "Setting usb${device_id} to "
-  echo "disabled" | sudo tee /sys/bus/usb/devices/usb${device_id}/power/wakeup
-done <<< "$(seq $device_range)"
+  echo "Disabling wake on USB device range ${device_range}."
+  echo "disabled" | sudo tee /sys/bus/usb/devices/${device_range}/power/wakeup > /dev/null
+done <<< "$device_ranges"
